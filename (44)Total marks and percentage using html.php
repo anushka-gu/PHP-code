38161ad50544ce76708fbe1cae2e5638f7cec256
ANUSHKA GUPTA <anushka.gupta863@gmail.com>
2025-04-11 00:22:36 +0530

    Update (44)Total marks and percentage using html.php

--- (44)Total marks and percentage using html.php
+++ (44)Total marks and percentage using html.php
@@ -79,7 +79,7 @@ $percentage = ($totalMarks / $maxMarks) * 100;
         </tr>
     </table>
 
-"This Program is written by Aaryan Goel 0221BCA163"
+"This Program is written by Anushka Gupta 0221BCA155"
 
 </body>
 </html>
