78461cecc6bd241e9fecdcd8143753483d812194
ANUSHKA GUPTA <anushka.gupta863@gmail.com>
2025-04-11 00:32:51 +0530

    Update (73)Bitwise operator.php

--- (73)Bitwise operator.php
+++ (73)Bitwise operator.php
@@ -11,5 +11,5 @@ echo "~a = " . (~$a) . "<br>"; // Bitwise NOT
 echo "a << 1 = " . ($a << 1) . "<br>"; // Left shift
 echo "a >> 1 = " . ($a >> 1) . "<br>"; // Right shift
 echo "<br>";
-echo "This Program is written by Aaryan Goel 0221BCA163"
+echo "This Program is written by Anushka Gupta 0221BCA155"
 ?>
