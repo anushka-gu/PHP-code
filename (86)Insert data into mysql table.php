55ba6b036e489c911f8fb38ceb8bb00c76f07214
ANUSHKA GUPTA <anushka.gupta863@gmail.com>
2025-04-11 00:37:54 +0530

    Update (86)Insert data into mysql table.php

--- (86)Insert data into mysql table.php
+++ (86)Insert data into mysql table.php
@@ -30,5 +30,5 @@ WAP in php to insert data into mysql table
     $conn->close();
 
     echo "<br>";
-    echo "This Program is written by Aaryan Goel 0221BCA163";
+    echo "This Program is written by Anushka Gupta 0221BCA155";
 ?>
