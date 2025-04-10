2a9421aa99b5697c8df7d20c87d889da062a4312
ANUSHKA GUPTA <anushka.gupta863@gmail.com>
2025-04-11 00:31:03 +0530

    Update (69)Delhi sunrise and sunset time.php

--- (69)Delhi sunrise and sunset time.php
+++ (69)Delhi sunrise and sunset time.php
@@ -22,6 +22,6 @@ WAP in php to demonstrate sunrise() and sunset() time of Delhi
 ?>
 
 <br>
-"This Program is written by Aaryan Goel 0221BCA163"
+"This Program is written by Anushka Gupta 0221BCA155"
 </body>
 </html>
