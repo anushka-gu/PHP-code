a6205f7ad0d7ab3d2afbecbee16e63754f9369f1
ANUSHKA GUPTA <anushka.gupta863@gmail.com>
2025-04-11 00:37:35 +0530

    Update (85)Alter a table.php

--- (85)Alter a table.php
+++ (85)Alter a table.php
@@ -56,5 +56,5 @@ if ($result->num_rows > 0) {
 $conn->close();
 
 echo "<br>";
-echo "This Program is written by Aaryan Goel 0221BCA163";
+echo "This Program is written by Anushka Gupta 0221BCA155";
 ?>
