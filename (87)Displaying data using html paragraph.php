64837b97e11ac01b5a852e60497b8ad6225a451f
ANUSHKA GUPTA <anushka.gupta863@gmail.com>
2025-04-11 00:38:17 +0530

    Update (87)Displaying data using html paragraph.php

--- (87)Displaying data using html paragraph.php
+++ (87)Displaying data using html paragraph.php
@@ -32,5 +32,5 @@ WAP in php to display data using html paragraph
     $conn->close();
 
 echo "<br>";
-echo "This Program is written by Aaryan Goel 0221BCA163";
+echo "This Program is written by Anushka Gupta 0221BCA155";
 ?>
